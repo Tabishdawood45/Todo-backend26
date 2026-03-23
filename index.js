05982deab2cb1b56ece05c2fad83d44b66edcf6d
Tabishdawood45 <Tabishdawood54@gmail.com>
2026-03-23 15:35:34 +0500

    update the file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 
-const todoRouter = require('./server/routes/todo')
+const todoRouter = require('./routes/todo')
 
 const app = express()
 
