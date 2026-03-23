063c78c72bd7afe9a1ef50e7737d68510b670eb1
Tabishdawood45 <Tabishdawood54@gmail.com>
2026-03-23 23:51:40 +0500

    task created auto

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-// TEMP: Create task table if it does not exist
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+
+// Import query from db.js
+const { query } = require('./helpers/db')
+
+const todoRouter = require('./routes/todo')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const port = process.env.PORT
+
+// ---------------- TEMP: Create task table if it does not exist ----------------
 const createTableQuery = `
 CREATE TABLE IF NOT EXISTS task (
     id SERIAL PRIMARY KEY,
@@ -8,32 +24,11 @@ CREATE TABLE IF NOT EXISTS task (
 
 query(createTableQuery)
   .then(() => console.log("Task table ready"))
-  .catch(err => console.log("Error creating table:", err));
-
-
-
-
-require('dotenv').config()
-
-const express = require('express')
-const cors = require('cors')
-
-const todoRouter = require('./routes/todo')
-
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-
-const port = process.env.PORT
-
+  .catch(err => console.log("Error creating table:", err))
 
 // USE ROUTES
 app.use("/", todoRouter)
 
-
-app.listen(port,()=>{
-
-console.log("Server running on port " + port)
-
+app.listen(port, () => {
+    console.log("Server running on port " + port)
 })
