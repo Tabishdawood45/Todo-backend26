e4d53d537702627ed85b7f9a903d92be3e91ea69
Tabishdawood45 <Tabishdawood54@gmail.com>
2026-03-24 00:30:04 +0500

    updated

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const express = require('express')
 const cors = require('cors')
 
 // Import query from db.js
-const { query } = require('./helpers/db')
 
 const todoRouter = require('./routes/todo')
 
@@ -15,16 +14,6 @@ app.use(express.json())
 const port = process.env.PORT
 
 // ---------------- TEMP: Create task table if it does not exist ----------------
-const createTableQuery = `
-CREATE TABLE IF NOT EXISTS task (
-    id SERIAL PRIMARY KEY,
-    description VARCHAR(255) NOT NULL
-);
-`;
-
-query(createTableQuery)
-  .then(() => console.log("Task table ready"))
-  .catch(err => console.log("Error creating table:", err))
 
 // USE ROUTES
 app.use("/", todoRouter)
