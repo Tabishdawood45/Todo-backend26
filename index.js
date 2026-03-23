2a3b71455309f8736f8dd328ed8fecfe8215867a
Tabishdawood45 <Tabishdawood54@gmail.com>
2026-03-24 02:34:27 +0500

    final update

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-// require('dotenv').config()
-
-// const express = require('express')
-// const cors = require('cors')
-
-// // Import query from db.js
-
-// const todoRouter = require('./routes/todo')
-
-// const app = express()
-// app.use(cors())
-// app.use(express.json())
-
-// const port = process.env.PORT || 10000;
-
-// // ---------------- TEMP: Create task table if it does not exist ----------------
-
-// // USE ROUTES
-// app.use("/", todoRouter)
-
-// app.listen(port, () => {
-//     console.log("Server running on port " + port)
-// })
-
-
-
-
 require('dotenv').config()
 // const { Pool } = require("pg");
 const express = require("express");
